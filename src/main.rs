a653a73756514c33e41bce6c3e3972cb781fe5ca
Orhan Aygün <orh.aygun@gmail.com>
2025-12-03 22:17:39 +0300

    step 3: list with state for menu

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,22 @@ use ratatui::{
     Terminal,
     backend::CrosstermBackend,
     layout::{Constraint, Layout},
-    widgets::{Block, Borders},
+    style::{Modifier, Style},
+    widgets::{Block, Borders, List, ListItem, ListState, Padding},
 };
 use std::io;
 
 pub struct App {
     notes: Vec<String>,
+    menu_state: ListState,
 }
 
 impl App {
     pub fn new() -> App {
-        App { notes: Vec::new() }
+        App {
+            notes: Vec::new(),
+            menu_state: ListState::default(),
+        }
     }
 
     pub fn new_with_dummy() -> App {
@@ -25,8 +30,13 @@ impl App {
             notes: vec![
                 "Note 1".to_string(),
                 "Note 2".to_string(),
-                "note 3".to_string(),
+                "Note 3".to_string(),
+                "Note 4".to_string(),
+                "Note 5".to_string(),
+                "Note 6".to_string(),
+                "Note 7".to_string(),
             ],
+            menu_state: ListState::default(),
         }
     }
 }
@@ -39,7 +49,8 @@ fn main() -> io::Result<()> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    let app = App::new_with_dummy();
+    let mut app = App::new_with_dummy();
+
     // 2. Main Loop
     loop {
         // Draw the UI
@@ -51,9 +62,29 @@ fn main() -> io::Result<()> {
             )
             .split(size);
 
-            let menu_block = Block::default().title(format!("Notes [{}]", app.notes.len())).borders(Borders::ALL);
-            f.render_widget(menu_block, layout[0]);
+            // # Menu
+            // ## Creating `ListItem`'s
+            let list_items: Vec<ListItem> = app
+                .notes
+                .iter()
+                .map(|note| ListItem::new(note.as_str()))
+                .collect();
 
+            // Creating `List` widget
+
+            let menu_block = Block::default()
+                .title(format!("Notes [{}]", app.notes.len()))
+                .padding(Padding::new(1, 1, 1, 1))
+                .borders(Borders::ALL);
+            let list = List::new(list_items).block(menu_block).highlight_style(
+                Style::default()
+                    .fg(ratatui::style::Color::Green)
+                    .add_modifier(Modifier::BOLD),
+            );
+
+            f.render_stateful_widget(list, layout[0], &mut app.menu_state);
+
+            // # Editor
             let editor_block = Block::default().title("Editor").borders(Borders::ALL);
             f.render_widget(editor_block, layout[1]);
         })?;
@@ -62,6 +93,12 @@ fn main() -> io::Result<()> {
         if let Event::Key(key) = event::read()? {
             if key.code == KeyCode::Char('q') {
                 break;
+            }
+
+            match key.code {
+                KeyCode::Down => app.menu_state.select_next(),
+                KeyCode::Up => app.menu_state.select_previous(),
+                _ => {}
             }
         }
     }
